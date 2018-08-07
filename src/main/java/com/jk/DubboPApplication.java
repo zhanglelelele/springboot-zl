092d7ebc0f21485b2194ec0ba68e9caea9ef4dfa
zhangle <77007602@qq.com>
2018-08-07 17:39:10 +0800

    first commit

---
+++ src/main/java/com/jk/DubboPApplication.java
@@ -0,0 +1,12 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboPApplication.class, args);
+	}
+}
